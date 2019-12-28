87ccf45bdab055290f5c0e25b071b5b47c730720
Matt Andrews <matt@carbonbasedcreative.com>
2019-12-28 11:16:02 -0700

    In class Assignment

---
+++ ReactWeek1Assignment.js
@@ -0,0 +1,27 @@
+//use strict;
+
+class Student {
+    constructor( name, email, community ) {
+        this.name = name;
+        this.email = email;
+        this.community = community;
+    }
+}
+
+class Bootcamp {
+    constructor( name, level, students = [] ) {
+        this.name = name;
+        this.level = level;
+        this.students = students;
+    }
+    registerStudent( student ) {
+        if ( this.students.filter(st => st.email === student.email) ) {
+            console.log(`${student.email} is already in our records for ${student.name}.`);
+        }
+        else {
+            this.students.push( student );
+            console.log(`Registering ${student.name} with email ${student.email} to the bootcamp ${this.name}.`);
+        }
+        return this.students;
+    }
+}
